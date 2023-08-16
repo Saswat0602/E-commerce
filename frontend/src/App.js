da55891911aee81accaa9cd70030f6ded874c616
saswat <saswatranjan0602@gmail.com>
2023-08-16 23:02:51 +0530

    st up

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+
+        <ToastContainer />
+      </>
+    </Router>
+  );
+}
+
+export default App;
