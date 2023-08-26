3df5edd76155f2b443c1d86f0366501daf7e0c88
saswat <saswatranjan0602@gmail.com>
2023-08-26 17:52:02 +0530

    filling up models field

--- backend/app/Models/PaymentDetails.php
+++ backend/app/Models/PaymentDetails.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class PaymentDetails extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'mobileNumber',
+        'alternateNumber',
+        'address',
+        'user_id', 
+        'order_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
